44c60081d1c6a07a4296650108601d5257da6904
thieflin <ignaciogarrido1234@hotmail.com>
2022-04-20 17:20:57 -0300

    Anda no lo puedo creer no se que hice es un milagro

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,42 +20,32 @@ public class Enemy : MonoBehaviourPun
     private void Start()
     {
         _hp = _maxHp;
-        slider.value = _maxHp;
-        slider.maxValue = _maxHp;
     }
 
 
-
-    public void OnTriggerEnter(Collider other)
-    {
-        //if (!pv.IsMine) return;
-        //if(other.gameObject.tag == "bullet")
-        //{
-        //    Destroy(other.gameObject);
-        //    pv.RPC("TakeDamage", RpcTarget.All, 20);
-        //}
-    }
-
-
-
+    //eSTA FUNCION ES LA QUE UPDATE EL SLIDER EN BASE A LA VIDA CON UN RPC PARA NOTIFICARLE A LOS OTROS LA VIDA QUE PIERDO
     [PunRPC]
     public void UpdateHp()
     {
             slider.value = _hp;
-        
     }
 
+    //Este take dmg lo que hace es updatear mi estado actual en MI pantalla
+    //Si no soy yo, lo que hace es decirle al otro, bueno maestro perdiste vida, y acordate de que
+    //tenes que cambiar el slider
     public void TakeDamage(int dmg)
     {
         if (!pv.IsMine)
         {
             _hp -= dmg;
             photonView.RPC("UpdateHp", RpcTarget.All);
+            //Bueno aca consulta si muere o no para hacer el pedidito de bicho
             if (_hp <= 0)
                 StartCoroutine(WaitTillSpawnCoroutine());
         }
         else
         {
+            //Si soy yo, entonces updateame a mi sin problemas
             _hp -= dmg;
             slider.value = _hp;
             if (_hp <= 0)
